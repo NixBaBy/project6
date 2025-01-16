f1df761b6d3656f7949fce5ebfbcd612806cfc4d
NixBaBy <tuuguu123123@gmail.com>
2025-01-16 10:29:09 +0800

    changes

--- src/components/ThridStep.jsx
+++ src/components/ThridStep.jsx
@@ -1,6 +1,7 @@
 import { Header } from "@/components/Header";
 import { Input } from "../components/Input";
 import { Button } from "@/components/Button";
+import { useState } from "react";
 export const ThridStep = ({
   setCurrentStep,
   currentStep,
@@ -9,6 +10,29 @@ export const ThridStep = ({
   formError,
   setFormValue,
 }) => {
+  const [image, setImage] = useState(null);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImage(URL.createObjectURL(file));
+      handlerChange({ target: { name: "image", value: file } });
+    }
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    if (file) {
+      setImage(URL.createObjectURL(file));
+      handlerChange({ target: { name: "image", value: file } });
+    }
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
   const handlerChange = (e) => {
     const { name, value } = e.target;
     setFormError((prev) => ({ ...prev, [name]: "" }));
@@ -16,12 +40,20 @@ export const ThridStep = ({
   };
   const clickHandler = () => {
     let errorHave = false;
-    const { date } = formValue;
+    const { date, image } = formValue;
 
     if (!date) {
       setFormError((prev) => ({
         ...prev,
         date: "enter your  date",
+      }));
+
+      errorHave = true;
+    }
+    if (!image) {
+      setFormError((prev) => ({
+        ...prev,
+        image: "enter your  image",
       }));
 
       errorHave = true;
@@ -42,6 +74,42 @@ export const ThridStep = ({
             error={formError.date}
           />
         </div>
+        <div
+          className="bg-[#f8f8f8] w-[392px] h-[180px] rounded-lg p-4 flex flex-col items-center justify-center relative"
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+        >
+          {image ? (
+            <img
+              src={image}
+              alt="Uploaded"
+              className="w-32 h-32 object-cover rounded-full"
+            />
+          ) : (
+            <>
+              <div className="bg-white p-2 rounded-full w-[28px] h-[28px]">
+                <img src="/imageIcon.svg" alt="" />
+              </div>
+              <p className="text-[#09090B] text-[14px]">Add image</p>
+              <input
+                type="file"
+                accept="image/*"
+                className="absolute inset-0 opacity-0 cursor-pointer"
+                onChange={handleImageChange}
+              />
+            </>
+          )}
+        </div>
+
+        <p
+          className={`${
+            formError.image
+              ? "visible text-red-500 text-[14px] focus:border-red-500 "
+              : "hidden"
+          }`}
+        >
+          {formError.image}
+        </p>
       </div>
       <Button
         setCurrentStep={setCurrentStep}
